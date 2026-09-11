512cc6fc871521cb16775d3e334dcea9bd5f1e2c
DrDead0 <112349103+DrDead0@users.noreply.github.com>
2026-09-11 05:44:01 +0530

    Non-Cmake Serach feature to search 2D array and into Objects

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,15 @@
 using namespace std;
 
 class Project {
+
 private:
+
     string name;
     string language;
     string type;
 
 public:
+
     Project(string projectName, string projectLanguage, string projectType) {
         name = projectName;
         language = projectLanguage;
@@ -28,26 +31,50 @@ public:
     string getType() const {
         return type;
     }
+
 };
 
 class ProjectManager {
+
 private:
+
     vector<Project> projects;
 
 public:
+
     void addProject(const Project& project) {
         projects.push_back(project);
     }
 
     void listProjects() {
+
         for (const Project& project : projects) {
+
             cout << project.getName() << endl;
+
         }
+
     }
 
     int getProjectCount() {
         return projects.size();
     }
+
+    Project* findProject(const string& projectName) {
+
+        for (Project& project : projects) {
+
+            if (project.getName() == projectName) {
+
+                return &project;
+
+            }
+
+        }
+
+        return nullptr;
+    }
+
 };
 
 int main() {
@@ -67,6 +94,28 @@ int main() {
     cout << "Total projects: "
          << manager.getProjectCount()
          << endl;
+
+    Project* foundProject = manager.findProject("Harbor");
+
+    if (foundProject != nullptr) {
+
+        cout << "Found project: "
+             << foundProject->getName()
+             << endl;
+
+        cout << "Language: "
+             << foundProject->getLanguage()
+             << endl;
+
+        cout << "Type: "
+             << foundProject->getType()
+             << endl;
+
+    } else {
+
+        cout << "Project not found." << endl;
+
+    }
 
     return 0;
 }
